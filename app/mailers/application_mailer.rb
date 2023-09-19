42bb8a41af3a630a5fbd8c3ecab6b34ce0988a46
clin2on3mun <munanaclinton@gmail.com>
2023-09-19 14:18:33 +0200

    Add data Models

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# This is your top-level documentation comment for the module.
+class ApplicationMailer < ActionMailer::Base
+  default from: 'from@example.com'
+  layout 'mailer'
+end
