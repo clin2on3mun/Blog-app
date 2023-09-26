14ab4b9c858871c6c470212a4ae672270ae507e9
clin2on3mun <munanaclinton@gmail.com>
2023-09-26 10:49:28 +0200

    Add controller test of users and posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+     describe 'GET/users/1/posts' do
+        it 'render a succesful response' do
+            get '/users/1/posts' 
+            expect(response).to be_successful
+        end
+        it 'render index action with index view' do
+            get '/users/1/posts'
+            expect(response.status).to eql(200)
+            expect(response.status).not_to eql(400)
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template('posts/index')
+            expect(response.body).to include ('List user posts')
+        end
+        it 'render show action with show view' do
+            get '/users/1/posts/3'
+            expect(response).to have_http_status(:success)
+            expect(response.status).to eql(200)
+            expect(response).to render_template('posts/show')
+            expect(response.body).to include ('Show Post by id')
+        end
+    end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+    describe 'GET/users' do
+        it 'renders a successful response' do
+            get '/users'
+            expect(response).to be_successful
+        end
+        it 'renders the index action with index view' do
+            get '/users'
+            expect(response).to have_http_status(:success)
+            expect(response.status).to eql(200)
+            expect(response.status).not_to eq(400)
+            expect(response).to render_template('users/index')
+            expect(response.body).to include('List of Users')
+        end
+        it 'renders the show action with show view' do
+            get '/users/1'
+            expect(response).to have_http_status(:success)
+            expect(response.status).to eq(200)
+            expect(response).to render_template('users/show')
+            expect(response.body).to include('user profile')
+        end
+    end
+end
