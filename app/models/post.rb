0750b9a5d730edec18509e66b36999b617de9f8a
clin2on3mun <munanaclinton@gmail.com>
2023-09-27 20:25:56 +0200

    Add block to index and show methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_post_comment
+  after_save :update_post_counter
   def recent_comments
     comments.order(created_at: :DESC).limit(5)
   end
