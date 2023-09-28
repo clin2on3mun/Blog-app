96934a7fee03b019c27ab7fc4514755f096078c7
clin2on3mun <munanaclinton@gmail.com>
2023-09-28 17:43:16 +0200

    Add controller and update like model

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author.id, id: @post.id), notice: 'comment created successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
