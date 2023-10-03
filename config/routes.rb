d95a7be464eab73a6eb642bebcce4ff621e784ed
clin2on3mun <munanaclinton@gmail.com>
2023-10-03 19:52:00 +0200

    Add changes to add email confirmation and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth',
-              path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   root 'users#index'
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resource :likes, only: [:create]
     end
   end
-
+    
   # Defines the root path route ("/")
   # root "articles#index"
 
