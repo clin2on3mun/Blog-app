b2cf6954d51673f83174e0461387a3931b90bf3e
clin2on3mun <munanaclinton@gmail.com>
2023-10-03 16:16:49 +0200

    Add devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth',
+              path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
