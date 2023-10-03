93c4f978f618a2d248b21559758a4af2e3cbf225
clin2on3mun <munanaclinton@gmail.com>
2023-10-03 16:35:49 +0200

    Fix linters of application

--- Gemfile
+++ Gemfile
@@ -72,4 +72,4 @@ group :test do
   gem 'selenium-webdriver'
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
 
   protected
-  
+
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
       user.permit(:name, :email, :password, :password_confirmation)
