5c132b1c86e00f34d82bd131b4b25318f2046a91
clin2on3mun <munanaclinton@gmail.com>
2023-10-05 20:09:53 +0200

    Fix the linters

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,21 +1,24 @@
 class Api::CommentsController < ApplicationController
   before_action :authenticate_user!
   def index
-      @post = Post.find(params[:post_id])
-      @comments = @post.comments
-      render json: @comments
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
   end
+
   def create
-      @comment = @post.comments.build(comment_params)
-      @comment.author = current_user
-      if @comment.save
-          render json: @comment
-      else
-          render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
-      end
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+    if @comment.save
+      render json: @comment
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
   end
+
   private
+
   def comment_params
-      params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text)
   end
 end
