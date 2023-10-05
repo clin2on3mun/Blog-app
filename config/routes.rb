93804ca664c7852f3e16ab007d76b56449c01464
clin2on3mun <munanaclinton@gmail.com>
2023-10-05 19:55:46 +0200

    Set up endpoints and generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,16 @@ Rails.application.routes.draw do
       resource :likes, only: [:create]
     end
   end
+
+  namespace :api, defaults: { format: 'json' } do
+    resources :users, only: [:show] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end    
+    end
+  
+  
     
   # Defines the root path route ("/")
   # root "articles#index"
