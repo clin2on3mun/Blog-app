6c17e4f673608985a02b9d58c594ea48a24c9b98
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-10-05 20:05:50 +0200

    Implement comments functionality with user authentication

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,3 +1,21 @@
 class Api::CommentsController < ApplicationController
-   
+  before_action :authenticate_user!
+  def index
+      @post = Post.find(params[:post_id])
+      @comments = @post.comments
+      render json: @comments
+  end
+  def create
+      @comment = @post.comments.build(comment_params)
+      @comment.author = current_user
+      if @comment.save
+          render json: @comment
+      else
+          render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
+  private
+  def comment_params
+      params.require(:comment).permit(:text)
+  end
 end
